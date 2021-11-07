a06fd62914e2d9abf4b3a64c4239ae8736516762
Dave Quinn <dquinn8@cogeco.ca>
2021-11-07 10:59:57 -0500

    improved README and added second screenshot

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
+    // The Network Path to Your MySQL Server
     host: 'localhost',
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
     password: 'MySQLpassword',
+    // Dont Change This
     database: 'employeeManagement'
 });
 
